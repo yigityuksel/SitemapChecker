979f2f1cc8a35b68a668c868eb8bd0659527b8d0
Yiğit Yüksel <yyu@emakina.com>
2019-08-01 09:47:51 +0300

    progress bar added.

--- crawl.py
+++ crawl.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import xmltodict
+from tqdm import tqdm
 from pandas import ExcelWriter
 
 def get_status(requestUrl):
@@ -17,10 +18,13 @@ print("Number of sitemaps:", len(data))
 df = pd.DataFrame(data, columns=["loc"])
 df['status'] = 0
 
-for index, row in df.iterrows():
-     df.set_value(index,'status', get_status(row['loc']))
-     print(row['loc'] + " --------- " + str(df.get_value(index, 'status')))
+with tqdm(total=len(data)) as pbar:
+     for index, row in df.iterrows():
+          df.set_value(index,'status', get_status(row['loc']))
+          #print(row['loc'] + " --------- " + str(df.get_value(index, 'status')))
+          pbar.update(1)
+          pbar.set_description("Processing sitemap")
 
-writer = pd.ExcelWriter('results.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('results.xlsx', engine='xlsxwriter',options={'strings_to_urls': False})
 df.to_excel(writer, sheet_name='Sheet1', index=False)
 writer.save()
